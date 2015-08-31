1e39cb6facf155d7d5ac846c6cf629b0d12fd9ce
CarlosDV <carlosarriagadv@gmail.com>
2015-08-31 02:50:01 +0200

    otus: prepare for orion

--- cm.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, device/motorola/otus/full_otus.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_otus
-PRODUCT_RELEASE_NAME := MOTO E 3G
