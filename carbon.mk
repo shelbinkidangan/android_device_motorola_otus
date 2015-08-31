a5df9f8d09c1d9bcbdda0bbc5d619653c6670276
CarlosDV <carlosarriagadv@gmail.com>
2015-08-31 04:58:56 +0200

    Update and rename cm.mk to carbon.mk

--- carbon.mk
+++ carbon.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_otus
+PRODUCT_NAME := carbon_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
